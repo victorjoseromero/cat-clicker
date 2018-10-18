66ee196c39a124c60b067a4bc68cf6cf6b2d3530
Victor Romero <vic.jose.romero@gmail.com>
2018-10-18 02:16:56 -0400

    Add files via upload

---
+++ catclicker.js
@@ -0,0 +1,24 @@
+var image = document.getElementById('my-elem');
+count = 0;
+image.addEventListener('click', ()=>{
+	count += 1; 
+	image.innerHTML = 'Number of Clicks: ' + count;
+
+	
+	
+	
+  //the element has been clicked... do stuff here
+}, false);
+
+
+
+
+
+
+};
+
+
+
+ 
+
+
